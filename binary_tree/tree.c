bdefefdc981bc4ab9bb5b615fa5b77f30d114284
Hua-Yuan <e61983+git@gmail.com>
2016-03-09 22:53:48 +0800

    implement show function to display the binary tree

--- binary_tree/tree.c
+++ binary_tree/tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node Node;
 
@@ -15,14 +16,32 @@ new(int data);
 static int
 add(pNode node, pNode root);
 
+static int
+show(pNode root);
+
 void
 unit_test();
 
 int main(int argc, const char *argv[])
 {
-    pNode test = new(3);
-    pNode test2 = new(4);
-    add(test, test2);
+    pNode node_3 = new(3);
+    pNode node_4 = new(4);
+    pNode node_7 = new(7);
+    pNode node_19 = new(19);
+    pNode node_25 = new(25);
+    pNode node_54 = new(54);
+    pNode node_52 = new(52);
+    pNode node_8 = new(8);
+    pNode node_15 = new(15);
+    add(node_3, node_19);
+    add(node_25, node_19);
+    add(node_7, node_19);
+    add(node_54, node_19);
+    add(node_4, node_19);
+    add(node_52, node_19);
+    add(node_8, node_19);
+    add(node_15, node_19);
+    show(node_19);
 #ifdef DEBUG
     unit_test();
 #endif
@@ -63,9 +82,26 @@ add(pNode node, pNode root)
     return 3;
 }
 
-void
-unit_test()
+static int
+show(pNode root)
 {
+    static int count = 1;
+    int current_count = count;
+    char showDot[current_count+1];
+    memset(showDot,'.',current_count);
+    showDot[0]='|';
+    showDot[current_count]='\0';
+    printf("%d\n",root->data);
+    count++;
+    if(NULL != root->left) {
+        printf("%sL",showDot);
+        show(root->left);
+    }
+    if(NULL != root->right) {
+        printf("%sR",showDot);
+        show(root->right);
+    }
+    return count--;
 }
 
 
