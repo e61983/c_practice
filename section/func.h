567f23dbf1349d6a5ccdcbb023811f98d57bc3cf
Yuan <e61983@gmail.com>
2019-09-17 23:10:15 +0800

    Let code more clear

--- section/func.h
+++ section/func.h
@@ -7,10 +7,18 @@ struct func_package {
     func_t func;
 };
 
-#define REGISTER_FUNC(f)                                               \
-    static struct func_package __func_info__##f                        \
-        __attribute((__section__("my_func"))) __attribute((__used__)) = { \
-            .name = #f,                                                 \
-            .func = f,                                                \
-    }
+extern void *__start_my_func;
+extern void *__stop_my_func;
 
+static void inline *get_first_package() { return &__start_my_func; }
+
+#define foreach_func(start)                                          \
+        for (struct func_package *i = (start); \
+             i < (struct func_package *)&__stop_my_func; i++)
+
+#define REGISTER_FUNC(f)                                                  \
+    static struct func_package __func_info__##f                           \
+        __attribute((__section__("my_func"))) __attribute((__used__)) = { \
+            .name = #f,                                                   \
+            .func = f,                                                    \
+    }
--- section/main.c
+++ section/main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "func.h"
-
-extern void *__start_my_func;
-extern void *__stop_my_func;
 
 int main(int argc, char *argv[])
 {
-    printf("start: %p\n", &__start_my_func);
-    printf("stop: %p\n", &__stop_my_func);
-
-    struct func_package *fp = (void*)&__start_my_func;
-    printf("name: %s\n", fp->name);
-    fp->func("1");
-    fp++;
-    printf("name: %s\n", fp->name);
-    fp->func("2");
+    struct func_package *fp = get_first_package();
+    foreach_func(fp) {
+        printf("name: %s\n", i->name);
+        i->func("1");
+    }
     return 0;
 }
