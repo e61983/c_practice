907e1147057f3a3499651d77a31fde0826c3a808
Yuan <e61983@gmail.com>
2019-09-17 23:32:19 +0800

    Avoid variable name conflict

--- section/func.h
+++ section/func.h
@@ -12,12 +12,15 @@ extern void *__stop_my_func;
 
 static void inline *get_first_package() { return &__start_my_func; }
 
-#define foreach_func(start)                                          \
-        for (struct func_package *i = (start); \
-             i < (struct func_package *)&__stop_my_func; i++)
+#define __COMBINE__(s1, s2) s1##_##s2
+#define COMBINE(s1, s2) __COMBINE__(s1, s2)
+
+#define foreach_func(start)                \
+    for (struct func_package *i = (start); \
+         i < (struct func_package *)&__stop_my_func; i++)
 
 #define REGISTER_FUNC(f)                                                  \
-    static struct func_package __func_info__##f                           \
+    static struct func_package COMBINE(__func_info__##f, __LINE__)        \
         __attribute((__section__("my_func"))) __attribute((__used__)) = { \
             .name = #f,                                                   \
             .func = f,                                                    \
